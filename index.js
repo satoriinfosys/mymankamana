d6029f2fc96a4c8d4e4153b05bb1d3492a4c9dac
satoriinfosys <satoriinfosys@gmail.com>
2023-10-15 18:31:41 +0545

    ch

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const { tokenValidator } = require('./middlewares/verify-token.middelware')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-app.use(cors())
+app.use(cors({
+    origin: '*',
+  }));
 app.use(fileUpload());
 
 app.use('/api/category', tokenValidator, require('./routes/category.route'))
