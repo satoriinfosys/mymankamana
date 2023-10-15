b3b7642cd87b41e9b630dbf183a0bdb29fe93d13
satoriinfosys <satoriinfosys@gmail.com>
2023-10-15 20:26:21 +0545

    cff

--- db/db.js
+++ db/db.js
@@ -1,22 +1,22 @@
 const mongoose = require("mongoose");
-const dbCred = process.env.MonngoDBuri;  // Ensure the correct environment variable name
+const dbCred = process.env.MonngoDBuri
 
 mongoose.set("strictQuery", false);
-mongoose.connect(dbCred, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
+mongoose.connect(dbCred,
+    {
+        useNewUrlParser: true
+    }
+);
 const db = mongoose.connection;
 
-db.on("error", function (error) {
-    console.error("Connection error:", error);
+db.on("error", function () {
+    console.error.bind(console, "connection error: ")
 });
 db.once("open", function () {
     console.log("Connected successfully");
 });
 
-module.exports = db;
+module.exports = db
 // const mongoose = require("mongoose");
 
 // // Get the MongoDB URI from environment variables
