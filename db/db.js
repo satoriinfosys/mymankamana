c05df766777b26bdf2c3b7f4a45dd5088363b675
satoriinfosys <satoriinfosys@gmail.com>
2023-10-15 20:48:44 +0545

    changes

--- db/db.js
+++ db/db.js
@@ -1,55 +1,55 @@
-const mongoose = require("mongoose");
-const dbCred = process.env.MonngoDBuri
-
-mongoose.set("strictQuery", false);
-mongoose.connect(dbCred,
-    {
-        useNewUrlParser: true
-    }
-);
-const db = mongoose.connection;
-
-db.on("error", function () {
-    console.error.bind(console, "connection error: ")
-});
-db.once("open", function () {
-    console.log("Connected successfully");
-});
-
-module.exports = db
 // const mongoose = require("mongoose");
+// const dbCred = process.env.MonngoDBuri
 
-// // Get the MongoDB URI from environment variables
-// const dbCred = process.env.MonngoDBuri;
-
-// // Check if the MongoDB URI is defined
-// if (!dbCred) {
-//     throw new Error('MongoDB URI is not set in environment variables.');
-// }
-
-// // Set mongoose option
 // mongoose.set("strictQuery", false);
-
-// // Connect to MongoDB
-// mongoose.connect(dbCred, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .catch(error => {
-//     console.error('Failed to connect to MongoDB:', error);
-//     process.exit(1);  // Exit the application if there's an error
-// });
-
-// // Set up event listeners for the mongoose connection
+// mongoose.connect(dbCred,
+//     {
+//         useNewUrlParser: true
+//     }
+// );
 // const db = mongoose.connection;
 
-// db.on("error", function (error) {
-//     console.error("Connection error:", error);
+// db.on("error", function () {
+//     console.error.bind(console, "connection error: ")
 // });
-
 // db.once("open", function () {
 //     console.log("Connected successfully");
 // });
 
-// // Export the mongoose connection
-// module.exports = db;
+// module.exports = db
+const mongoose = require("mongoose");
+
+// Get the MongoDB URI from environment variables
+const dbCred = process.env.MonngoDBuri;
+
+// Check if the MongoDB URI is defined
+if (!dbCred) {
+    throw new Error('MongoDB URI is not set in environment variables.');
+}
+
+// Set mongoose option
+mongoose.set("strictQuery", false);
+
+// Connect to MongoDB
+mongoose.connect(dbCred, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.catch(error => {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1);  // Exit the application if there's an error
+});
+
+// Set up event listeners for the mongoose connection
+const db = mongoose.connection;
+
+db.on("error", function (error) {
+    console.error("Connection error:", error);
+});
+
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+// Export the mongoose connection
+module.exports = db;
