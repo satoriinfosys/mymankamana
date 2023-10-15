13a1bfed7bf7b140cdc276c743ff71d8c53c345c
satoriinfosys <satoriinfosys@gmail.com>
2023-10-15 18:48:30 +0545

    cha

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const dotenv = require('dotenv').config()
 const express = require('express')
 const moment = require('moment')
-const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+const PORT = process.env.port || 5000;
 const app = express()
 const conn = require('./db/db')
 var cors = require('cors')
