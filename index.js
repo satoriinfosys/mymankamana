52e2ddca34948ebc8b7a626b43506358c8dd37b9
satoriinfosys <satoriinfosys@gmail.com>
2024-03-06 17:12:03 +0545

    cha

--- index.js
+++ index.js
@@ -88,21 +88,21 @@ app.get('/fetch-resource', async (req, res) => {
 });
 
 // Your other routes
-app.use('/api/category', tokenValidator, require('./routes/category.route'));
-app.use('/api/team', tokenValidator, require('./routes/team.route'));
-app.use('/api/notice', tokenValidator, require('./routes/notice.route'));
-app.use('/api/event', tokenValidator, require('./routes/event.route'));
-app.use('/api/trip', tokenValidator, require('./routes/trip.route'));
-app.use('/api/query', tokenValidator, require('./routes/query.route'));
-app.use('/api/response', tokenValidator, require('./routes/response.route'));
-app.use('/api/booking', tokenValidator, require('./routes/booking.route'));
-app.use('/api/user', tokenValidator, require('./routes/user.route'));
-app.use('/api/blog', tokenValidator, require('./routes/blog.route'));
-app.use('/api/guidline', tokenValidator, require('./routes/guidline.route'));
-app.use('/api/subscription', tokenValidator, require('./routes/subscription.route'));
-app.use('/api/master', tokenValidator, require('./routes/masterData.route'));
-app.use('/api/media', tokenValidator, require('./routes/media.route'));
-app.use('/api/authentication', tokenValidator, require('./routes/authentication.route'));
+app.use('/api/category', tokenValidator, require('./routes/category.route.js'));
+app.use('/api/team', tokenValidator, require('./routes/team.route.js'));
+app.use('/api/notice', tokenValidator, require('./routes/notice.route.js'));
+app.use('/api/event', tokenValidator, require('./routes/event.route.js'));
+app.use('/api/trip', tokenValidator, require('./routes/trip.route.js'));
+app.use('/api/query', tokenValidator, require('./routes/query.route.js'));
+app.use('/api/response', tokenValidator, require('./routes/response.route.js'));
+app.use('/api/booking', tokenValidator, require('./routes/booking.route.js'));
+app.use('/api/user', tokenValidator, require('./routes/user.route.js'));
+app.use('/api/blog', tokenValidator, require('./routes/blog.route.js'));
+app.use('/api/guidline', tokenValidator, require('./routes/guidline.route.js'));
+app.use('/api/subscription', tokenValidator, require('./routes/subscription.route.js'));
+app.use('/api/master', tokenValidator, require('./routes/masterData.route.js'));
+app.use('/api/media', tokenValidator, require('./routes/media.route.js'));
+app.use('/api/authentication', tokenValidator, require('./routes/authentication.route.js'));
 
 Master_model.find({}, function (err, result) {
   if (result.length == 0) {
