5f9e8b02515dbd26e723f0d3e13859aa35c89b4c
satoriinfosys <satoriinfosys@gmail.com>
2024-03-06 12:43:09 +0545

    chan

--- db/db.js
+++ db/db.js
@@ -19,70 +19,43 @@
 
 
 
-// // module.exports = db
-// const mongoose = require("mongoose");
+// module.exports = db
+const mongoose = require("mongoose");
 
-// // Get the MongoDB URI from environment variables
-// const dbCred = process.env.MonngoDBuri;
+// Get the MongoDB URI from environment variables
+const dbCred = process.env.MonngoDBuri;
 
-// // Check if the MongoDB URI is defined
-// if (!dbCred) {
-//     throw new Error('MongoDB URI is not set in environment variables.');
-// }
-
-// // Set mongoose option
-// mongoose.set("strictQuery", false);
-
-// // Connect to MongoDB
-// mongoose.connect(dbCred, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// })
-// .catch(error => {
-//     console.error('Failed to connect to MongoDB:', error);
-//     process.exit(1);  // Exit the application if there's an error
-// });
-
-// // Set up event listeners for the mongoose connection
-// const db = mongoose.connection;
-
-// db.on("error", function (error) {
-//     console.error("Connection error:", error);
-// });
-
-// db.once("open", function () {
-//     console.log("Connected successfully");
-// });
-
-// // Export the mongoose connection
-// module.exports = db;
-
-
-
-const express = require('express')
-const mongoose = require('mongoose')
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
+// Check if the MongoDB URI is defined
+if (!dbCred) {
+    throw new Error('MongoDB URI is not set in environment variables.');
 }
 
-//Routes go here
-app.all('*', (req,res) => {
-    res.json({"every thing":"is awesome"})
-})
+// Set mongoose option
+mongoose.set("strictQuery", false);
 
-//Connect to the database before listening
-connectDB().then(() => {
-    app.listen(PORT, () => {
-        console.log("listening for requests");
-    })
+// Connect to MongoDB
+mongoose.connect(dbCred, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.catch(error => {
+    console.error('Failed to connect to MongoDB:', error);
+    process.exit(1);  // Exit the application if there's an error
+});
+
+// Set up event listeners for the mongoose connection
+const db = mongoose.connection;
+
+db.on("error", function (error) {
+    console.error("Connection error:", error);
+});
+
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
+// Export the mongoose connection
+module.exports = db;
+
+
+
