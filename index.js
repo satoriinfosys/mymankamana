ebd4d68afad747d2f294eb4db23b7d906d0c4020
satoriinfosys <satoriinfosys@gmail.com>
2024-03-06 14:28:11 +0545

    ch

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
    origin: '*' ,
-  }));
+   headers: {
+    "Access-Control-Allow-Origin": "*"
+  }}));
 app.use(fileUpload());
 
 app.use('/api/category', tokenValidator, require('./routes/category.route'))
