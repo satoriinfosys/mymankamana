83ac470c55318da8bcbcff6e5dac49b48fd62836
satoriinfosys <satoriinfosys@gmail.com>
2024-03-06 14:05:13 +0545

    added varcel.json

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { tokenValidator } = require('./middlewares/verify-token.middelware')
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
-   origin: 'https://www.mymanakamanatravels.com',
+   origin: 'https://www.mymanakamanatravels.com' ,
   }));
 app.use(fileUpload());
 
