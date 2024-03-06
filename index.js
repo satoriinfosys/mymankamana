9f665b252b6c4d3d550bdeec616851771ac8200c
satoriinfosys <satoriinfosys@gmail.com>
2024-03-06 14:43:40 +0545

    ch

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors({
    origin: '*' ,
-   headers: {
+   "headers": {
     "Access-Control-Allow-Origin": "https://www.mymanakamanatravels.com"
   }}));
 app.use(fileUpload());
