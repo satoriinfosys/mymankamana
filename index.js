58f72d414815cf0c6ece92be8987a673fc6431b6
satoriinfosys <satoriinfosys@gmail.com>
2024-03-11 01:08:00 +0545

    chhhh

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const fileUpload = require("express-fileupload");
 const { Master_model } = require('./models/materData.model.js');
 const { tokenValidator } = require('./middlewares/verify-token.middelware');
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+}); 
+
 // Express app configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
