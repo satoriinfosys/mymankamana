0403fe22e5aa6ed1c4452dbaf538d21b1de38b12
satoriinfosys <satoriinfosys@gmail.com>
2024-03-10 23:22:48 +0545

    ch

--- index.js
+++ index.js
@@ -70,6 +70,7 @@ app.use(cors({
   origin: ['*', 'https://www.mymanakamanatravels.com']
 }));
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Origin', 'https://www.mymanakamanatravels.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
