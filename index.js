c08d5e685dd26ab14ca09000382e67c4cd3b167b
satoriinfosys <satoriinfosys@gmail.com>
2024-03-11 10:33:28 +0545

    changes

--- index.js
+++ index.js
@@ -1,79 +1,85 @@
-// Import required modules
-import { S3Client } from '@aws-sdk/client-s3';
-import dotenv from 'dotenv';
-import express from 'express';
-import moment from 'moment';
-import mongoose from 'mongoose';
-import fileUpload from 'express-fileupload';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-
-// Load environment variables
-dotenv.config();
-
-// Initialize Express app
-const app = express();
+import { S3Client } from '@aws-sdk/client-s3'
+const dotenv = require('dotenv').config();
+const express = require('express');
+const moment = require('moment');
 const PORT = process.env.PORT || 3000;
+const mongoose = require('mongoose');
+const app = express();
+const connectToDatabase = require('./db/db.js');
+const cors = require('cors');
+const fileUpload = require("express-fileupload");
+const { Master_model } = require('./models/materData.model.js');
+const { tokenValidator } = require('./middlewares/verify-token.middelware');
+const client = new S3Client({ region: process.env.AWS_BUCKET_REGION });
 
-// Create an instance of the AWS S3 client
-const s3Client = new S3Client({ region: process.env.AWS_BUCKET_REGION });
+var bodyParser = require('body-parser');
 
-// Middleware
-app.use(cors());
+// Enables CORS for all routes
+app.use(cors({ origin: '*' }));
+
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   if (req.method === "OPTIONS") {
+//     return res.sendStatus(200);
+//   }
+//   next();
+// }); 
+
+// Express app configuration
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload());
 
 // Define routes
-const routes = [
-  '/api/category',
-  '/api/team',
-  '/api/notice',
-  '/api/event',
-  '/api/trip',
-  '/api/query',
-  '/api/response',
-  '/api/booking',
-  '/api/user',
-  '/api/blog',
-  '/api/guideline',
-  '/api/subscription',
-  '/api/master',
-  '/api/media',
-  '/api/authentication'
-];
+app.use('/api/category', tokenValidator, require('./routes/category.route'));
+app.use('/api/team', tokenValidator, require('./routes/team.route'));
+app.use('/api/notice', tokenValidator, require('./routes/notice.route'));
+app.use('/api/event', tokenValidator, require('./routes/event.route'));
+app.use('/api/trip', tokenValidator, require('./routes/trip.route'));
+app.use('/api/query', tokenValidator, require('./routes/query.route'));
+app.use('/api/response', tokenValidator, require('./routes/response.route'));
+app.use('/api/booking', tokenValidator, require('./routes/booking.route'));
+app.use('/api/user', tokenValidator, require('./routes/user.route'));
+app.use('/api/blog', tokenValidator, require('./routes/blog.route'));
+app.use('/api/guidline', tokenValidator, require('./routes/guidline.route'));
+app.use('/api/subscription', tokenValidator, require('./routes/subscription.route'));
+app.use('/api/master', tokenValidator, require('./routes/masterData.route'));
+app.use('/api/media', tokenValidator, require('./routes/media.route'));
+app.use('/api/authentication', tokenValidator, require('./routes/authentication.route'));
 
-routes.forEach(route => {
-  app.use(route, tokenValidator, require(`./routes${route}.route`));
-});
 
-// Connect to database
 connectToDatabase()
   .then(() => {
     console.log('Connected to database');
-    // Add any server setup code here
+    // Add your server setup and start code here
   })
-  .catch(error => {
+  .catch((error) => {
     console.error('Failed to connect to database:', error);
     process.exit(1);
   });
 
-// Check and save data in Master_model if necessary
-Master_model.find({}, (err, result) => {
-  if (result.length === 0) {
-    const futureDate = moment().add(15, 'days').toDate();
-    const masterTable = new Master_model({ 'createdon': futureDate });
-    masterTable.save((error, savedResult) => {
-      if (error) {
-        console.error('Error occurred while saving data:', error);
-      }
-    });
-  }
+Master_model.find({}, function (err, result) {
+    if (result.length == 0) {
+        var futureDate = moment().add(15, 'days');
+        var toDate = futureDate.toDate();
+        Master_table = new Master_model({ 'createdon': toDate });
+        Master_table.save((error, savedResult) => {
+            if (error) {
+                console.error('Error occurred while saving data:', error);
+            }
+        });
+    }
 });
 
 // Start the Express server
 app.listen(PORT, () => {
-  console.log(`Server running at port: ${PORT}`);
+    console.log(`Server running at port : ${PORT}`);
 });
 
 // Export the Express app wrapped in a serverless function
